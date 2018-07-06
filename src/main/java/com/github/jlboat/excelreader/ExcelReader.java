eb74cb54ff0edefc309c2fe4165e48b11a1f79fb
Lucas Boatwright <lboat@localhost.localdomain>
2018-07-06 15:11:35 -0400

    Initial commit

---
+++ src/main/java/com/github/jlboat/excelreader/ExcelReader.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.jlboat.excelreader;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+
+/**
+ *
+ * @author J. Lucas Boatwright
+ */
+public class ExcelReader {
+    public static void main(String[] args){
+        Workbook wb = new HSSFWorkbook();
+        try (OutputStream fileOut = new FileOutputStream(args[0])){
+            wb.write(fileOut);
+        } catch (IOException ex){
+            System.err.println("Error writing file");
+        }// end try-catch
+    }
+}
